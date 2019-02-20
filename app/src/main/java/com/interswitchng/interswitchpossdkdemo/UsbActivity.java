44817369ea727b50f9e4d9a598f5c0dec20b022e
PaskyAdigun <adigunhammed.lekan@gmail.com>
2019-02-20 11:24:52 +0000

    USB and Android communication impl

--- app/src/main/java/com/interswitchng/interswitchpossdkdemo/UsbActivity.java
+++ app/src/main/java/com/interswitchng/interswitchpossdkdemo/UsbActivity.java
@@ -5,28 +5,47 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
-import com.interswitchng.interswitchpossdk.shared.PosUsbManager;
+import com.interswitchng.smartpos.usb.UsbConnectionManager;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class UsbActivity extends AppCompatActivity {
 
-    private PosUsbManager manager;
+    private UsbConnectionManager usbConnectionManager;
+    private ExecutorService executorService = Executors.newCachedThreadPool();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_usb);
 
-        manager = new PosUsbManager(this);
+        usbConnectionManager = new UsbConnectionManager();
+        executorService.execute(() -> {
+
+            boolean opened = usbConnectionManager.open();
+            log("Opened => " + opened);
+            while (true) {
+                String data = usbConnectionManager.receive();
+                log(data);
+
+                usbConnectionManager.send("Received => " + data);
+
+                if (data.equalsIgnoreCase("writing 4")) {
+                    break;
+                }
+            }
+        });
+
+        setContentView(R.layout.activity_demo);
 
     }
 
     private void log(String m) {
-        Log.d("USB", m);
+        Log.d("USBTest", m);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        manager.connectAccessory(getIntent());
     }
 }
