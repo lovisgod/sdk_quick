e259277970119c0d99ebbfe35babe9a5d1b2f3c3
Biodun <talk2olatejuemmanuel@gmail.com>
2019-09-25 12:03:33 +0100

    Update UI design to print receipt

--- smart-pos-core/src/main/java/com/interswitchng/smartpos/modules/main/settings/ActivityDetailFragment.kt
+++ smart-pos-core/src/main/java/com/interswitchng/smartpos/modules/main/settings/ActivityDetailFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.interswitchng.smartpos.R
+import kotlinx.android.synthetic.main.isw_activity_detail.*
 
 class ActivityDetailFragment : Fragment() {
 
@@ -14,5 +16,16 @@ class ActivityDetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.isw_activity_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        handlePrintReceiptClick()
+    }
+
+    private fun handlePrintReceiptClick() {
+        isw_print_receipt_label.setOnClickListener {
+            it.findNavController().navigate(R.id.isw_action_goto_fragment_receipt)
+        }
     }
 }
