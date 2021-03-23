7c7430c727388226d477c291e344f577ada2d80b
Richard.Ali <richard.ali@interswitchgroup.com>
2021-03-23 10:02:41 +0100

    fixed network selector

--- smart-pos-core/src/main/java/com/interswitchng/smartpos/modules/main/billPayment/adapters/NetworkRecyclerAdapter.kt
+++ smart-pos-core/src/main/java/com/interswitchng/smartpos/modules/main/billPayment/adapters/NetworkRecyclerAdapter.kt
@@ -11,16 +11,29 @@ import com.interswitchng.smartpos.R
 import com.interswitchng.smartpos.modules.main.billPayment.models.NetworkListCallBackListener
 
 class NetworkRecyclerAdapter(val networkList: Array<String>, private val callBackListener: NetworkListCallBackListener<Array<String>, View>): RecyclerView.Adapter<NetworkRecyclerAdapter.ViewHolder>() {
+    var viewHolderList = arrayListOf<ViewHolder>()
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NetworkRecyclerAdapter.ViewHolder {
         val v = LayoutInflater.from(parent.context).inflate(R.layout.isw_network_list_card, parent, false)
-        v.setOnClickListener {
-            callBackListener.onDataReceived(arrayOf(""), v)
-        }
+        viewHolderList.add(ViewHolder(v))
         return ViewHolder(v)
+    }
+
+    fun updateBackground() {
+        for(viewHolder in viewHolderList) {
+            if (viewHolder !=null) {
+               val z = viewHolder.itemView.findViewById(R.id.isw_netowrk_card_container) as FrameLayout
+               z.setBackgroundResource(0)
+            }
+        }
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bindItems(networkList[position])
+        holder.itemView.setOnClickListener {
+            updateBackground()
+            val frame = holder.itemView.findViewById(R.id.isw_netowrk_card_container) as FrameLayout
+            frame.setBackgroundResource(R.drawable.isw_recharge_networks_bg)
+        }
     }
 
     override fun getItemCount(): Int {
@@ -33,15 +46,15 @@ class NetworkRecyclerAdapter(val networkList: Array<String>, private val callBac
         fun bindItems(network: String) {
             itemView.setOnClickListener {
 //              add the drawable background to the frame
-                val frame = itemView.findViewById(R.id.isw_netowrk_card_container) as FrameLayout
-                Log.d("Frame Click", frame.id.toString())
-                previous.let {
-                    if (it != null) {
-                        it.setBackgroundResource(0)
-                    }
-                }
-                previous = frame
-                frame.setBackgroundResource(R.drawable.isw_recharge_networks_bg)
+//                val frame = itemView.findViewById(R.id.isw_netowrk_card_container) as FrameLayout
+//                Log.d("Frame Click", frame.id.toString())
+//                previous.let {
+//                    if (it != null) {
+//                        it.setBackgroundResource(0)
+//                    }
+//                }
+//                previous = frame
+//                frame.setBackgroundResource(R.drawable.isw_recharge_networks_bg)
             }
         }
     }
